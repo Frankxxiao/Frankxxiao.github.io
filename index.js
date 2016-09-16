0a8587688cd3bfae23a17e6b554f42efb8085d05
ShawFrank <frank@ShawFrankdeMacBook-Air.local>
2016-09-16 15:22:11 +0800

    adding angularJS

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var server = require("./server");
+// var server = require("./server");
+
+// server.start();
 
 function IndexController($scope){
-	$scope.start = server.start();
+	$scope.start = function() {
+	  function onRequest(request, response) {
+	    console.log("Request received.");
+	    response.writeHead(200, {"Content-Type": "text/plain"});
+	    response.write("Hello World");
+	    response.end();
+	  }
+
+	  http.createServer(onRequest).listen(8888);
+	  console.log("Server has started.");
+	}
 }
